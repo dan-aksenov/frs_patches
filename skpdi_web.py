ee0cd848ba0c9bbc7316f0eddc44cd96b0630e39
dan-aksenov <dan-aksenov@yandex.ru>
2018-06-29 16:38:49 +0300

    web adjusted

--- skpdi_web.py
+++ skpdi_web.py
@@ -9,9 +9,9 @@ def check_webpage(patch_num, application_host, target):
     page = requests.get('http://' + application_host + ':8080/' + target)
     if page.status_code <> 200:
        print Bcolors.WARNING + "WARNING: Application webpage unnaccesseble: " + str(page.status_code) + "\n" + Bcolors.ENDC
-    elif 'ver-' + patch_num in page.text:
+    elif 'ver-' + patch_num + '.ico' in page.text:
         print Bcolors.OKGREEN + "SUCCESS: Application webpage matches " + patch_num + "\n" + Bcolors.ENDC
-    elif 'ver-' + patch_num not in page.text:
+    elif 'ver-' + patch_num + '.ico' not in page.text:
         print Bcolors.WARNING + "WARNING: Application webpage not matches " + patch_num + "\n" + Bcolors.ENDC
     else:
         print Bcolors.WARNING + "WARNING: Problem determining application version.\n" + Bcolors.ENDC
