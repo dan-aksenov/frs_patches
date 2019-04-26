5cae2fda282543755cca461703ddbf1c2c7f1440
dan-aksenov <dan-aksenov@yandex.ru>
2019-04-26 13:20:33 +0300

    var "target" changed to "app_context"

--- skpdi_web.py
+++ skpdi_web.py
@@ -1,17 +1,15 @@
 import requests
 from utils import Bcolors
 
-def check_webpage(patch_num, application_host, target):
-    # redo it with /u01/apache-tomcat-8.5.23/webapps/record/META-INF/maven/ru.fors.ods/record/pom.xml version check?
-	# cos it'll also work on pts
-    ''' Seek version name in web page's code. '''
+def check_webpage(patch_num, application_host, app_context):
+    ''' Seek version name in SKPDI's web page's code. '''
 
-    page = requests.get('http://' + application_host + ':8080/' + target)
+    page = requests.get('http://' + application_host + ':8080/' + app_context)
     if page.status_code <> 200:
-       print Bcolors.WARNING + "WARNING: Application " + target + " on " + application_host + " is unnaccesseble: " + str(page.status_code) + "\n" + Bcolors.ENDC
+       print Bcolors.WARNING + "WARNING: Application " + app_context + " on " + application_host + " is unnaccesseble: " + str(page.status_code) + "\n" + Bcolors.ENDC
     elif 'ver-' + patch_num + '.ico' in page.text:
-        print Bcolors.OKGREEN + "SUCCESS: Application " + target + " on " + application_host + " matches " + patch_num + " version\n" + Bcolors.ENDC
+        print Bcolors.OKGREEN + "SUCCESS: Application " + app_context + " on " + application_host + " matches " + patch_num + " version\n" + Bcolors.ENDC
     elif 'ver-' + patch_num + '.ico' not in page.text:
-        print Bcolors.WARNING + "WARNING: Application " + target + " on " + application_host + " not matches " + patch_num + " version\n" + Bcolors.ENDC
+        print Bcolors.WARNING + "WARNING: Application " + app_context + " on " + application_host + " not matches " + patch_num + " version\n" + Bcolors.ENDC
     else:
-        print Bcolors.WARNING + "WARNING: Problem determining application version for" + target +" on " + application_host + ".\n" + Bcolors.ENDC
+        print Bcolors.WARNING + "WARNING: Problem determining application version for" + app_context +" on " + application_host + ".\n" + Bcolors.ENDC
